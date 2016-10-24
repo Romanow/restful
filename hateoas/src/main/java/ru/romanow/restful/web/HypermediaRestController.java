766f07cf3c7b01a2ba60b21c0a6b68ca0a2f4067
Romanow <romas@1c.ru>
2016-10-24 17:49:18 +0300

    add two links

--- hateoas/src/main/java/ru/romanow/restful/web/HypermediaRestController.java
+++ hateoas/src/main/java/ru/romanow/restful/web/HypermediaRestController.java
@@ -1,12 +1,12 @@
 package ru.romanow.restful.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.hateoas.EntityLinks;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.romanow.restful.domain.Server;
 import ru.romanow.restful.web.model.ServerResource;
 import ru.romanow.restful.web.service.ServerService;
@@ -18,23 +18,25 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 /**
  * Created by ronin on 22.10.16
  */
-@ExposesResourceFor(ServerResource.class)
-@RepositoryRestController
+@RestController
 @RequestMapping("/server")
 public class HypermediaRestController {
 
     @Autowired
     private ServerService serverService;
 
+    @Autowired
+    private EntityLinks entityLinks;
+
     @GetMapping("/{id}")
     public ResponseEntity<ServerResource> getServer(@PathVariable Integer id) {
         ServerResource server = serverService.getById(id);
-        server.add(linkTo(HypermediaRestController.class).withSelfRel());
+        server.add(linkTo(HypermediaRestController.class).slash(id).withSelfRel());
+        server.add(linkTo(HypermediaRestController.class).withRel("list"));
         return ResponseEntity.ok(server);
     }
 
     @GetMapping
-    @RequestMapping("/list")
     public ResponseEntity<List<Server>> getServers() {
         return ResponseEntity.ok(serverService.findAll());
     }
