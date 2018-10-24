69db342622c2472b2bb4c1010982728fc954e237
a.romanov <a.romanov@softpro.com>
2018-10-24 16:18:13 +0300

    remove HAL browser

--- src/main/java/ru/romanow/restful/RestApplication.java
+++ src/main/java/ru/romanow/restful/RestApplication.java
@@ -3,9 +3,7 @@ package ru.romanow.restful;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Created by romanow on 02.09.16
- */
+// HAL browser docker run -p8081:80 -e ENTRY_POINT=http://localhost:8880/hateoas/server jcassee/hal-browser
 @SpringBootApplication
 public class RestApplication {
 
