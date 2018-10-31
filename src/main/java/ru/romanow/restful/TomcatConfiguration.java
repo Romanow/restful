189aede7f3f93ab082424d343722744f34679ba2
a.romanov <a.romanov@softpro.com>
2018-10-31 17:59:18 +0300

    ssl

---
+++ src/main/java/ru/romanow/restful/TomcatConfiguration.java
@@ -0,0 +1,24 @@
+package ru.romanow.restful;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfiguration {
+
+    @Value("${server.http.port}")
+    private int httpPort;
+
+    @Bean
+    public TomcatServletWebServerFactory containerCustomizer() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(httpPort);
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
+    }
+}
