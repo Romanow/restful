0b508c132338d10df5a2e3d52694f0c1d7d28f36
a.romanov <a.romanov@softpro.com>
2018-11-14 13:39:27 +0300

    fix run

--- src/main/java/ru/romanow/restful/RestApplication.java
+++ src/main/java/ru/romanow/restful/RestApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // HAL browser docker run -p8081:80 -e ENTRY_POINT=http://localhost:8880/hateoas/server jcassee/hal-browser
+// java -jar build/libs/restful-1.0.0.jar --server.port=8081
 @SpringBootApplication
 public class RestApplication {
 
