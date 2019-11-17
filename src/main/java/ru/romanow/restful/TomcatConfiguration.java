112e0e82a4ccb255ac0dc3c3cb8c96a1f008d9c8
Romanow Alex <romanowalex@hotmail.com>
2019-11-17 23:36:17 +0300

    use http2

--- src/main/java/ru/romanow/restful/TomcatConfiguration.java
+++ src/main/java/ru/romanow/restful/TomcatConfiguration.java
@@ -1,11 +1,11 @@
 package ru.romanow.restful;
 
 import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http2.Http2Protocol;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class TomcatConfiguration {
@@ -16,6 +16,7 @@ public class TomcatConfiguration {
     @Bean
     public TomcatServletWebServerFactory containerCustomizer() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.addUpgradeProtocol(new Http2Protocol());
         connector.setPort(httpPort);
 
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
